fa1c397f0519843ba5dda929aa143da88dfcdaf3
Danil Lyashenko <shub21680@gmail.com>
2019-11-20 23:18:10 +0200

    add example of using callback

--- example.js
+++ example.js
@@ -5,6 +5,7 @@ let bot = new TelegramBot(tokenConfig.token);
 
 bot.setPolling((type, message) => {
   console.log(type, message);
+  bot.sendMessage("your id", JSON.stringify(message));
 });
 
 bot.sendMessage("your id", "It's working", data => {
